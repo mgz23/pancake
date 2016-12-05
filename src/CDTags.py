2551bc0c166b48e8dbd6f3846f85636f963f577b
Michał Pasternacki <michal.jaroslaw.pasternacki@gmail.com>
2016-12-05 21:26:26 +0100

    CDTags.py retrieves data for multi-disc releaes and returns output in dict

--- src/CDTags.py
+++ src/CDTags.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import musicbrainzngs, discid
 
 musicbrainzngs.set_useragent("pancakerip", "0.01", "arturdwieszopy@gmail.com")
 
+disc_info = {}
 
 disc_id = discid.read()
 full_data = musicbrainzngs.get_releases_by_discid(disc_id, includes=['artists', 'recordings', 'artist-credits'])['disc']['release-list'][0]
 
-artist = full_data['artist-credit'][0]['artist']['name']
-if artist == "Various Artists":
-    artist = {}
+disc_info['artist'] = full_data['artist-credit'][0]['artist']['name']
+if disc_info['artist'] == "Various Artists":
+    disc_info['artist'] = {}
     for i in full_data['medium-list'][0]['track-list']:
-        artist[i['number']] = i['artist-credit-phrase']
-album = full_data['title']
-year = full_data['date']
-total_tracks = full_data['medium-list'][0]['disc-list'][0]['offset-count']
-tracks = {}
+        disc_info['artist'][i['number']] = i['artist-credit-phrase']
+disc_info['year'] = full_data['date']
+disc_info['title'] = full_data['title']
+disc_info['total_tracks'] = full_data['medium-list'][0]['disc-list'][0]['offset-count']
+disc_info['total_discs'] = full_data['medium-count']
 
-for i in full_data['medium-list'][0]['track-list']:
-    tracks[i['number']] = i['recording']['title']
+disc_info['tracks'] = {}
+
+if disc_info['total_discs'] > 1:
+
+    toc = []
+    for i in disc_id.toc_string.split(" ")[3:]:
+        toc.append(int(i))
+
+    for i in full_data['medium-list']:
+        if i ['disc-list'][0]['offset-list'] == toc:
+            disc_data = i
+
+    for i in disc_data['track-list']:
+        disc_info['tracks'][i['number']] = i['recording']['title']
+
+    disc_info['disc_number'] = disc_data['position']
+    disc_info['disc_title'] = disc_data['title']
+    disc_info['total_tracks'] = disc_data['disc-list'][0]['offset-count']
+
+else:
+    
+    for i in full_data['medium-list'][0]['track-list']:
+        disc_info['tracks'][i['number']] = i['recording']['title']
+
+
